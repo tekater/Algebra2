5e27f3abe3565dbea7c0f7aca06ee370a58956ac
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 19:56:53 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,13 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("a * x = b");
+        System.out.println(" ");
         System.out.println("Введите число a:");// 5
         double a = scanner.nextDouble();
         System.out.println("Введите число b:");// 3
         double b = scanner.nextDouble();
         System.out.println(a+ " * x = " + b);
+        System.out.println(" ");
         double x = (b / a);
         System.out.println("x=" + x);
         System.out.println(" ");
