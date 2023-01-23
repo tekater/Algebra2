4367ccb053e381149e0dfdcdeb33eb45c7f2328a
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 19:55:39 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a * x = b");
+        System.out.println("Введите число a:");// 5
+        double a = scanner.nextDouble();
+        System.out.println("Введите число b:");// 3
+        double b = scanner.nextDouble();
+        System.out.println(a+ " * x = " + b);
+        double x = (b / a);
+        System.out.println("x=" + x);
+        System.out.println(" ");
+    }
+}
