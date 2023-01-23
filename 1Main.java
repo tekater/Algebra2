5aedc453c47e9bcc7964ccf2ba510ef7f4a81757
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 20:02:04 +0900

    Update 1Main.java

--- 1Main.java
+++ 1Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("a * x = b");
         System.out.println(" ");
